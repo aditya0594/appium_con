fda70ff5ef0a7d73737d7e642d28abe473fced10
Aditya Pawar <aditya.pawar@pragmatyc.com>
2024-06-30 15:56:39 +0530

    First commit

---
+++ src/test/java/Pages/BaseTest.java
@@ -0,0 +1,94 @@
+package Pages;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+
+public class BaseTest {
+
+	public AndroidDriver driver;
+	String path;
+	public AppiumDriverLocalService service;
+
+	@BeforeClass
+	public void ConfigureAppium() throws MalformedURLException
+	{
+		
+		/* service = new AppiumServiceBuilder().withAppiumJS(new File("C://Users//Aditya Pawar//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+			service.start();
+			*/
+		 path = System.getProperty("user.dir");
+			UiAutomator2Options options = new UiAutomator2Options();
+			//options.setDeviceName("RahulPhone"); //emulator
+			options.setDeviceName("RZ8NA1P2S8D");// real device
+			options.setChromedriverExecutable(path+"//Users//rahulshetty//documents//chromedriver 11");
+			
+			options.setApp(path+"//src/test/java/resources/ApiDemos-debug.apk");
+		//	options.setApp("//Users//rahulshetty//workingcode//Appium//src//test//java//resources//General-Store.apk");
+			
+			 driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
+			 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	public void longPressAction(WebElement ele)
+	{
+		((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
+				ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),
+						"duration",2000));
+	}
+	
+	public void scrollToEndAction()
+	{
+		boolean canScrollMore;
+		do
+		{
+		 canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+			    "left", 100, "top", 100, "width", 200, "height", 200,
+			    "direction", "down",
+			    "percent", 3.0
+			    
+			));
+		}while(canScrollMore);
+	}
+	
+	
+	public void swipeAction(WebElement ele,String direction)
+	{
+		((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+				"elementId", ((RemoteWebElement)ele).getId(),
+			 
+			    "direction", direction,
+			    "percent", 0.75
+			));
+		
+		
+	}
+	
+	
+	public Double getFormattedAmount(String amount)
+	{
+		Double price = Double.parseDouble(amount.substring(1));
+		return price;
+		
+	}
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+        //service.stop();
+		}
+	
+}
